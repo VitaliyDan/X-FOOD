eef978b487137708e8c8ef662554c62c02e52b6b
VitaliyDanaykanich <vitailyc.ua@gmail.com>
2022-09-29 20:53:38 +0300

    some upgrade

--- js/modules/cards.js
+++ js/modules/cards.js
@@ -20,14 +20,15 @@ function cards(){
 
             element.innerHTML = `
                 <div class="menu__item">
-                    <img src=${this.src} alt=${this.altimg} style="height:300px; object-fit: cover;">
+                    <div>
+                        <img src=${this.src} alt=${this.altimg} style="height:300px; object-fit: cover;">
+                    </div>
                     <h3 class="menu__item-subtitle">${this.title}</h3>
                     <div class="menu__item-descr">${this.descr}</div>
                     <div class="menu__item-divider"></div>
                     <div class="menu__item-price">
-                        <div class="menu__item-cost">Price:</div>
-                        <div class="menu__item-total"><span>${this.price}</span> UAH/day</div>
-                    </div>
+                    <div class="menu__item-cost">Price:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> UAH/day</div>                       
                 </div>
             `;
             this.parent.append(element);
