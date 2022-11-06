d9c9395e04f09f911b77a619f73c05bdb9c19341
Pavlo Yakhontov <yakhontovpavel@gmail.com>
2022-11-06 20:38:07 +0200

    auth

--- js/modules/signInCallBack.js
+++ js/modules/signInCallBack.js
@@ -1,15 +1,14 @@
 
-async function login(event) {
+async function signIn(event) {
     event.preventDefault()
     const inputs = Array.from(event.target.querySelectorAll('.reg'));
     const loginData = {};
     for (const input of inputs) {
-        console.log('inputs: ', input.value);
         loginData[input.name] = input.value;
     }
     console.log(loginData);
 
-    fetch('http://localhost:3000/users', {
+    fetch('http://localhost:3000/login', {
         method: "POST",
         body: JSON.stringify(loginData),
         headers: {
